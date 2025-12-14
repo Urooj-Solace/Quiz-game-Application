3224355750b024ba996626fb9340a0b2f4898a0e
Aman Javed <amancui2722@gmail.com>
2025-12-14 22:17:03 +0500

    Update QuizApp.java
    Till Add Questions

--- QuizApp.java
+++ QuizApp.java
@@ -62,5 +62,83 @@ public class Test1 {
             }
         }
     }
+  public static void adminLogin() {
+        System.out.print("Enter Admin Username: ");
+        String user = input.nextLine();
 
+        System.out.print("Enter Admin Password: ");
+        String pass = input.nextLine();
+
+        if (user.equals(ADMIN_USER) && pass.equals(ADMIN_PASS)) {
+            adminSubjectMenu();
+        } else {
+            System.out.println("Incorrect username or password!");
+        }
+    }
+
+    public static void adminSubjectMenu() {
+        while (true) {
+            System.out.println("\n--- CHOOSE SUBJECT ---");
+            System.out.println("1. Islamiyat");
+            System.out.println("2. Math");
+            System.out.println("3. English");
+            System.out.println("4. Science");
+            System.out.println("5. Computer");
+            System.out.println("6. Back");
+            System.out.print("Enter choice: ");
+
+            String ch = input.nextLine();
+
+            switch (ch) {
+                case "1":
+                    adminPanel(islamiat, "Islamiyat", ISLAM_FILE);
+                    break;
+                case "2":
+                    adminPanel(math, "Math", MATH_FILE);
+                    break;
+                case "3":
+                    adminPanel(english, "English", ENGLISH_FILE);
+                    break;
+                case "4":
+                    adminPanel(science, "Science", SCIENCE_FILE);
+                    break;
+                case "5":
+                    adminPanel(computer, "Computer", COMPUTER_FILE);
+                    break;
+                case "6":
+                    return;
+                default:
+                    System.out.println("Invalid choice!");
+            }
+        }
+    }
+
+    public static void adminPanel(String[][] arr, String subjectName, String fileName) {
+        while (true) {
+            System.out.println("\n--- ADMIN PANEL (" + subjectName + ") ---");
+            System.out.println("1. Add Question");
+            System.out.println("2. View Questions");
+            System.out.println("3. Update Question");
+            System.out.println("4. Back");
+            System.out.print("Enter choice: ");
+
+            String ch = input.nextLine();
+
+            switch (ch) {
+                case "1":
+                    addQuestion(arr, subjectName, fileName);
+                    break;
+                case "2":
+                    viewQuestions(arr, subjectName);
+                    break;
+                case "3":
+                    updateQuestion(arr, subjectName, fileName);
+                    break;
+                case "4":
+                    return;
+                default:
+                    System.out.println("Invalid choice!");
+            }
+        }
+    }
   
